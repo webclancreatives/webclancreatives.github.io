e4e15af592ed7d5b9532b836c6b6b45131c97f5d
webclancreatives <webclancreatives@gmail.com>
2018-10-29 07:56:49 +0530

    Created Contact Page

--- script.js
+++ script.js
@@ -5,11 +5,12 @@ AOS.init({
 var navi = document.getElementsByClassName("nav")[0];
 var cont = document.getElementsByClassName("content");
 var butt = document.getElementsByClassName("but");
+var soc = document.getElementsByClassName("socnet")[0];
 
 var mobnavi = document.getElementById("mnav");
 var clsbtn = document.getElementsByClassName("closebtn")[0];
 
-window.onscroll = function() {
+window.onscroll = function navig() {
 	if (document.body.scrollTop > 140 || document.documentElement.scrollTop > 140) {
 		navi.style.background = "#eaeaea";
 		navi.style.color = "#dc162d"
@@ -21,6 +22,16 @@ window.onscroll = function() {
 		navi.style.background = "#333333";
 		navi.style.color = "#ffd700";
 		navi.style.position = "static";
+	}
+
+	if (document.body.scrollTop > 512 || document.documentElement.scrollTop > 512) {
+		soc.style.position = "fixed";
+		soc.style.top = "70px";
+	}
+
+	else if (document.body.scrollTop < 511 || document.documentElement.scrollTop < 511) {
+		soc.style.position = "absolute";
+		soc.style.top = "512px";
 	}
 }
 
